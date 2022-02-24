cf2baf60f6de759088968f92b1f5f21129794b8a
thomasravache <thomasravache31@gmail.com>
2022-02-24 18:12:09 -0300

    Requisito 07 e 08 - OK

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,13 +7,13 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS,
+  timeout: process.env.REACT_APP_TIMEOUT,
 };
-
+// TIMEOUT 300000
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
